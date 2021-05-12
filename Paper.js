e51c33b39d9105571fae870df0e2438c7f51cbc5
Parshva2007 <81500194+Parshva2007@users.noreply.github.com>
2021-05-12 13:05:04 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,34 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+        var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+        }
+		this.x=x;
+        this.r=r;
+		this.y=y;
+		this.image=loadImage("paper.png")
+		this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options)
+        World.add(world,this.body)
+	}
+	display()
+	{
+	        var paperpos=this.body.position
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);	
+			fill(255,0,56)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r,this.r)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+			
+	}
+
+}
